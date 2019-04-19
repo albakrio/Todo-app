ccfb9f93dbc819233075f55dad89b2f4db8d8dbc
OMAR AL-BAKRI <goldozil@OMARs-MacBook-Pro-2.local>
2019-04-19 15:41:42 -0400

    added a new const

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import IndecisionApp from './components/IndecisionApp'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
+const Omar = 1
+
 
 ReactDOM.render(<IndecisionApp />,document.getElementById('app'))
 
